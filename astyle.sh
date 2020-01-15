0c8bbc35ab6d802f43abfdd955496d7fb1607732
Jonas Claeson <jonas.cl@protonmail.com>
2020-01-15 12:03:51 +0100

    Add a script that formats each port with astyle

---
+++ astyle.sh
@@ -0,0 +1,17 @@
+#/bin/sh
+
+#
+# Script that formats each port with astyle
+#
+
+SRC_DEMOS=$(find Demo/ -name '*.[ch]' | grep -v "^Demo/Common")
+SRC_PORTS=$(find Source/portable/ -name '*.[ch]' | grep -v "^Source/portable/MemMang")
+
+astyle \
+    --max-instatement-indent=100 \
+    --style=otbs \
+    --pad-header \
+    --indent=spaces=4 \
+    --pad-oper \
+    ${SRC_DEMOS} \
+    ${SRC_PORTS}
