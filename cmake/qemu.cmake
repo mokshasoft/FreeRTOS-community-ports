532443877a230dfa24a2c14ff0ed6a1f2cf5fb5b
Jonas Claeson <jonas.cl@protonmail.com>
2020-01-23 11:15:55 +0100

    Use qemu, gdbserver and gdb CMake target for versatilepb demo

--- cmake/qemu.cmake
+++ cmake/qemu.cmake
@@ -6,15 +6,13 @@
 # See "LICENSE_BSD2.txt" for details.
 #
 
-function(create_qemu_target target)
-    assert(PLATFORM "Variable PLATFORM has to be set to use create_qemu_target")
-
+function(create_qemu_target platform target)
     # Run the app in QEMU
     add_custom_target(qemu.${target} DEPENDS ${target})
     add_custom_command(
         TARGET qemu.${target}
         POST_BUILD
-        COMMAND qemu-system-arm -M ${PLATFORM} -m 128M -nographic -kernel ${CMAKE_BINARY_DIR}/images/${target}.bin
+        COMMAND qemu-system-arm -M ${platform} -m 128M -nographic -kernel ${CMAKE_BINARY_DIR}/images/${target}.bin
         USES_TERMINAL
     )
     # Start qemu and open a gdbserver on TCP port 1234
@@ -22,7 +20,7 @@ function(create_qemu_target target)
     add_custom_command(
         TARGET gdbserver.${target}
         POST_BUILD
-        COMMAND qemu-system-arm -M ${PLATFORM} -m 128M -nographic -kernel ${CMAKE_BINARY_DIR}/images/${target}.bin -s -S
+        COMMAND qemu-system-arm -M ${platform} -m 128M -nographic -kernel ${CMAKE_BINARY_DIR}/images/${target}.bin -s -S
         USES_TERMINAL
     )
     # Start gdb and break on _start
@@ -30,7 +28,7 @@ function(create_qemu_target target)
     add_custom_command(
         TARGET gdb.${target}
         POST_BUILD
-        COMMAND ${CROSS_COMPILER_PREFIX}-gdb -ex 'target remote :1234' -ex 'b _start' ${target}
+        COMMAND ${CROSS_COMPILER_PREFIX}-gdb -ex 'target remote :1234' -ex 'b main' ${target}
         USES_TERMINAL
     )
 endfunction()
