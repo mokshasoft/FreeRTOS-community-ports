1761b74e31e494e72a03c39e0846f3893e01f690
Jonas Claeson <jonas.cl@protonmail.com>
2020-01-27 16:12:23 +0100

    Create a project that builds Idris apps on FreeRTOS

---
+++ Demo/ARM926ejs-GCC-Idris/Utils.idr
@@ -0,0 +1,24 @@
+{-
+Copyright 2020, Mokshasoft AB (mokshasoft.com)
+
+This software may be distributed and modified according to the terms of
+the BSD 2-Clause license. Note that NO WARRANTY is provided.
+See "LICENSE_BSD2.txt" for details.
+-}
+
+module Utils
+
+%access public export
+%default total
+
+%include C "print.h"
+
+-- int16_t printInit(uint16_t uart_nr);
+printInit : Int -> IO Int
+printInit uart_nr = 
+    foreign FFI_C "printInit" (Int -> IO Int) uart_nr
+
+-- void vDirectPrintMsg(const portCHAR* msg);
+vDirectPrintMsg : String -> IO ()
+vDirectPrintMsg msg =
+    foreign FFI_C "vDirectPrintMsg" (String -> IO ()) msg
---
+++ cmake/Idris.cmake
@@ -0,0 +1,33 @@
+#
+# Copyright 2020, Mokshasoft AB (mokshasoft.com)
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+
+cmake_minimum_required(VERSION 3.7.2)
+
+# Test that the Idris version is greater than version
+function(test_idris_version version)
+    execute_process(
+        COMMAND idris --version
+        OUTPUT_VARIABLE IDRIS_CMD_VERSION
+    )
+    string(
+        REGEX MATCH [0-9]+\.[0-9]+.[0-9]+
+        IDRIS_VERSION ${IDRIS_CMD_VERSION}
+    )
+    if(${IDRIS_VERSION} VERSION_LESS ${version})
+        message(FATAL_ERROR "This project needs at least Idris version ${version}")
+    endif()
+endfunction()
+
+# Transcompile Idris files to C
+function(idris_tc_files target idris_main)
+    add_custom_command(
+        OUTPUT ${target}
+        COMMAND idris -i ${ARGN} --codegen C --codegenonly -o ${CMAKE_CURRENT_BINARY_DIR}/${target} ${idris_main}
+        DEPENDS ${idris_main}
+    )
+endfunction()
