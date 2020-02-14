bf095c908bb3c0bc1264937b4140c9e72515d2e0
Jonas Claeson <jonas.cl@protonmail.com>
2020-02-14 07:49:15 +0100

    Revert back to the old bare-metal implementation

--- Libraries/idris-rts/rts/bare-metal/idris_main.c
+++ Libraries/idris-rts/rts/bare-metal/idris_main.c
@@ -2,9 +2,6 @@
 #include "idris_opts.h"
 #include "idris_rts.h"
 #include "idris_stats.h"
-#include "print.h"
-#include <FreeRTOS.h>
-#include <task.h>
 
 void _idris__123_runMain_95_0_125_(VM* vm, VAL* oldbase);
 
@@ -14,38 +11,21 @@ RTSOpts opts = {
     .show_summary   = 0
 };
 
-static void uartLog(const char* msg) {
-    vDirectPrintMsg(msg);
-}
-
-static void halt() {
-    for (; ;);
-}
-
-void vRootThread(void* pvParameters) {
+int main() {
     VM* vm = init_vm(opts.max_stack_size, opts.init_heap_size, 1);
     init_gmpalloc();
     init_nullaries();
+
     _idris__123_runMain_95_0_125_(vm, NULL);
-}
 
-int main() {
-    // Init print
-    if (pdFAIL == printInit(0)) {
-        uartLog("Init print failed\r\n");
-        halt();
+#ifdef IDRIS_DEBUG
+    if (opts.show_summary) {
+        idris_gcInfo(vm, 1);
     }
+#endif
 
-    // Create root thread
-    if (pdPASS != xTaskCreate(vRootThread, "root", 2000, NULL, 0, NULL)) {
-        uartLog("Failed to create root thread\r\n");
-        halt();
-    }
+    // Remove call to terminate since it crashes the application during a free
+    //Stats stats = terminate(vm);
 
-    // Start the FreeRTOS scheduler
-    vTaskStartScheduler();
-
-    // Will not get here unless there is insufficient RAM
-    uartLog("will not get here\r\n");
     return EXIT_SUCCESS;
 }
